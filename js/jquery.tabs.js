a92798557f9a3513363ce66aff9cf71a9b801240
Rain Zhai <zhaiyu963@gmail.com>
2013-07-30 18:32:06 +0800

    tabs in navigation.html

--- js/jquery.tabs.js
+++ js/jquery.tabs.js
@@ -1,42 +1,35 @@
-$.extend({
-		rainTab : function(o) {
-				o = $.extend({                  
-					tabSelector:".c_tab",
-					panelSelector:"#tab",
-					tabEvent:"click",
-					styleClass:"currentLink",
-					hoverClass:"itemLightGrey"
-				}, o || {});
-				//tab 的钩子
-				var temp_tab = o.tabSelector,
-				//受控的id开头
-				temp_panel = o.panelSelector,
-				tabsize = $(temp_tab).length,
-				//tab触发事件
-				tabE = o.tabEvent,
-				//当前tablink的样式类
-				tabClass = o.styleClass,
-				//未选中tablink的hover类
-				hoverClass = o.hoverClass;
-				
-				$(temp_tab).bind(tabE,function(){  
-						if(tabsize>0){
-							$(temp_tab).children().removeClass(tabClass);
-							$(this).children().addClass(tabClass);						
-							$(temp_tab).children().addClass(hoverClass);
-							$(this).children().removeClass(hoverClass);	
-							showTab($(this).index(),tabsize);
-						}
-				});
-				
-				function showTab(num,tabsize){
-						for(var i=0;i<tabsize;i++){  
-								if(num===i){
-									$(temp_panel+'-'+i).removeClass("hide");  
-								}else{
-									$(temp_panel+'-'+i).addClass("hide");  
-								}
-						}
-				}  
-		}
+$.fn.extend({
+	tabs : function(obj) {
+		var o = $.extend({
+			tabSelector:null,
+			panelSelector:null,
+			'event':"click",
+			currentClass:'',
+			hoverClass:''
+		}, obj || {});
+		var _this =$(this),
+		//tab 的钩子
+		temp_tab = o.tabSelector || 'li',
+		//受控的id开头
+		temp_panel = o.panelSelector || 'div',
+		tabsize = $(temp_tab).length,
+		//tab触发事件
+		tabE = o.event || 'click',
+		//当前tablink的样式类
+		tabClass = o.currentClass,
+		//未选中tablink的hover类
+		hoverClass = o.hoverClass;
+		_this.children(temp_panel).hide();
+		$(_this.children(temp_panel)[0]).show();
+		_this.find(temp_tab).bind(tabE,function(){  
+				if(tabsize>0){
+					$(temp_tab).removeClass(tabClass).addClass(hoverClass);
+					$(this).addClass(tabClass).removeClass(hoverClass);	
+					var i = $(this).index();
+					_this.children(temp_panel).hide();
+					$(_this.children(temp_panel)[i]).show();
+				}
+		});	
+		return _this;
+	}
 });
