2375abe4a685da518121a8d1a10c155e24808ba5
Rain Zhai <zhaiyu963@gmail.com>
2015-01-01 18:50:15 +0800

    app数据更新

--- app/js/app/data/gamelist.js
+++ app/js/app/data/gamelist.js
@@ -1,17 +1,24 @@
 //gamelist data
 var data = {
 	gamelist:[{
-		url:'http://wande.me/gg/index.html',
-		imgurl:'http://wande.me/game/images/gameicons-2.jpg',
-		name:'帅哥爱消除',
-		intro:'一款帅哥消除游戏',
+		url:'http://play.famobi.com/highjump/A1000-1',
+		imgurl:'http://wande.me/games/wp-content/themes/NextWPA/timthumb.php?src=http://wande.me/games/wp-content/uploads/2014/10/xingtiaoyue.jpg',
+		name:'超级猩跳跃',
+		intro:'猩猩跳跃的游戏',
 		start:'开始游戏'
 	},
 	{
-		url:'http://wande.me/mm/index.html',
-		imgurl:'http://wande.me/game/images/gameicons-3.jpg',
-		name:'美女爱消除',
-		intro:'一款美女消除游戏',
+		url:'http://codecanyon.net/item/pet-party-columns-html5-game/full_screen_preview/6651091',
+		imgurl:'http://wande.me/games/wp-content/themes/NextWPA/timthumb.php?src=http://wande.me/games/wp-content/uploads/2014/09/gamepet20140920174018.jpg',
+		name:' 宠物派对',
+		intro:' 宠物消除欢乐多',
+		start:'开始游戏'
+	},
+	{
+		url:'http://go.weizoo.com/flappy/',
+		imgurl:'http://wande.me/game/images/gameicons-6.jpg',
+		name:'Flappy bird',
+		intro:'虐心变态小鸟',
 		start:'开始游戏'
 	},
 	{
