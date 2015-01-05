6c411bcc7c4018c110a86bf51af7602c5754f84b
zhaiyu <zhaiyu963@gmail.com>
2015-01-05 17:53:26 +0800

    cdn更新

--- app/js/app/view/searchHead.js
+++ app/js/app/view/searchHead.js
@@ -7,7 +7,7 @@ define(['template'],function (t) {
 		'<div class="search posr bgw tal h-3 wf-90 b mt">'+
 			'<input id="searchbox" class="searchbox wf-90 nop nob" type="text" placeholder="搜索" maxlength="18" />'+
 			'<span id="clearbtn" class="clearbtn block w-2 h-2 bglgrey roundall posa tac grey hide">x</span>'+
-			'<a id="searchbtn" class="searchicon ib fsm ts h-2 posa pl" href="javascript:;"><i class="icon-search"></i></a>'+
+			'<a id="searchbtn" class="searchicon ib fsm ts h-2 posa pl" href="javascript:;"><svg class="h-2 w-2"><use xlink:href="#icon-search"></use></svg></a>'+
 		'</div>'+
 	'</div>'+
 	'</div>'+
